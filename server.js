5db6e6ac32a83837b1cf4a90a311b1d7e494c803
Nithin134 <133798305+Nithin134@users.noreply.github.com>
2025-07-16 14:41:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,58 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import compression from 'compression'; 
 
 import authRoutes from './routes/auth.js';
 import roomRoutes from './routes/rooms.js';
 import expenseRoutes from './routes/expenses.js';
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "https://splitaaara.netlify.app", credentials: true }));
+
+app.use(compression());
+
+
+app.use(cors({
+  origin: "https://splitaaara.netlify.app",
+  credentials: true
+}));
+
+
 app.use(express.json());
 
-console.log("🟡 Connecting to MongoDB...");
+app.use(express.static("public", {
+  setHeaders: (res) => {
+    res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+  }
+}));
 
-mongoose.connect('mongodb+srv://myAtlasDBUser:Sorry.2.kill@myatlasclusteredu.8vpwie9.mongodb.net/', {
+console.log(" Connecting to MongoDB...");
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://myAtlasDBUser:Sorry.2.kill@myatlasclusteredu.8vpwie9.mongodb.net/', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 }).then(() => {
   console.log(' MongoDB connected');
 
-  // Only start server AFTER DB is connected
+
   app.use('/api/auth', authRoutes);
   app.use('/api/rooms', roomRoutes);
   app.use('/api/expenses', expenseRoutes);
 
+
   app.get('/', (req, res) => {
-    res.send('Welcome to the Room Expense Splitter API');
+    res.send('Welcome to the Room Expense Splitter API ');
   });
 
-  process.on('uncaughtException', (err) => {
-  console.error('🔥 Uncaught Exception:', err);
+
+  app.use((err, req, res, next) => {
+    console.error(" Uncaught error:", err.stack);
+    res.status(500).json({ message: "Internal Server Error" });
   });
 
 
@@ -41,12 +60,6 @@ mongoose.connect('mongodb+srv://myAtlasDBUser:Sorry.2.kill@myatlasclusteredu.8vp
     console.log(` Server running on port ${PORT}`);
   });
 
-    app.use((err, req, res, next) => {
-      console.error(" Uncaught error:", err.stack);
-      res.status(500).json({ message: "Server crashed" });
-  });
-
-
 }).catch(err => {
-  console.error('❌ MongoDB connection error:', err);
+  console.error(' MongoDB connection error:', err);
 });
